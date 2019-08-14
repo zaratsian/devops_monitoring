875ea03cbf1383cf39212625ee98a888f7dd82ba
Dan Zaratsian <dtz001@gmail.com>
2019-08-14 19:41:33 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "Name prefix for the nodes"
+  default     = "dzgce"
+}
+
+variable "num_nodes" {
+  description = "Number of nodes to create"
+  default     = 50
+}
